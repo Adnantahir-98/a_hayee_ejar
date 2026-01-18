a93fecbf9f3d8cf41a2614e550a2d0a088d67cd9
Dev Ninja <codeveloper101@gmail.com>
2026-01-18 22:50:22 +0500

    test

--- app/app-pages/selected-result-page/main.js
+++ app/app-pages/selected-result-page/main.js
@@ -54,6 +54,7 @@ export default function SelectedResultPage() {
       handleWhatsappShow(); // Opens your existing modal
     }
   };
+  
   const handleCallClick = () => {
     const phoneNumber = phone; // Replace with actual number
 
